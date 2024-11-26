bd10d6d86fd59591a0148f320ce3f1ebda2a86f8
Broderline1 <532310054@correcaminos.uts.edu.mx>
2024-11-26 02:01:10 -0700

    Ultimos detalles

--- src/app/components/usuario-list/usuario-list.component.ts
+++ src/app/components/usuario-list/usuario-list.component.ts
@@ -8,27 +8,44 @@ import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
 import { UserModel } from '../../models/user.model';
 import { UsuarioService } from '../../services/usuario/usuario.service';
+import { Password } from 'primeng/password';
 
 @Component({
   selector: 'app-usuario-list',
   standalone: true,
   imports: [ButtonModule, DropdownModule, DialogModule, TableModule, FormsModule, ReactiveFormsModule],
   templateUrl: './usuario-list.component.html',
+  animations:[
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition(':enter', [
+        animate('500ms 0s ease-in')
+      ]),
+      transition(':leave', [
+        animate('500ms 0s ease-out', style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ],
   styleUrl: './usuario-list.component.css'
 })
 export class UsuarioListComponent {
   usuarios: UserModel[] = [];
   displayDialog: boolean = false;
   usuarioForm!: FormGroup;
+  isEditing : boolean = false;
   
   constructor(private fb: FormBuilder, private usuarioService: UsuarioService) {}
 
   ngOnInit(){
     this.usuarioForm = this.fb.group({
       IdUsuario: [null],
-      nombre: ['', Validators.required],
-      usuario: ['', Validators.required],
-      contrasena: ['', Validators.required],
+      names: ['', Validators.required],
+      users: ['', Validators.required],
+      password: ['', Validators.required],
     });
 
     this.loadUsuarios();
@@ -41,11 +58,62 @@ export class UsuarioListComponent {
     });
   }
 
-  openDialog(){
+  saveUsuario() {
+    if(this.usuarioForm.valid){
+      const usuarioData = this.usuarioForm.value;
+      if(this.isEditing){
+        this.updateUsuario(usuarioData);
+      } else {
+        this.createUsuario(usuarioData);
+      }
+    }
+  }
+
+  openDialog(usuario?: any){
     this.displayDialog = true;
+    this.isEditing = usuario;
+    if(usuario) {
+      this.usuarioForm.patchValue({
+        IdUsuario: usuario.idUsuario,
+        names: usuario.nombre,
+        users: usuario.usuario,
+        password: usuario.contrasena,
+      });
+    } else {
+      this.usuarioForm.reset();
+    }
   }
 
   closeDialog(){
     this.displayDialog = false;
   }
+
+  createUsuario(usuario: any) {
+    this.usuarioService.createUsuario({
+      nombre: usuario.names,
+      usuario: usuario.users,
+      contrasena: usuario.password ,
+    }).subscribe((data) => {
+      this.loadUsuarios();
+      this.displayDialog = false;
+    })
+  }
+
+  updateUsuario(usuario: any) {
+    this.usuarioService.updateUsuario(usuario.IdUsuario, {
+      nombre: usuario.names,
+      usuario: usuario.users,
+      contrasena: usuario.password,
+    }).subscribe((data) => {
+      this.loadUsuarios();
+      this.displayDialog = false;
+    })
+  }
+
+  deleteUsuario(id: number) {
+    this.usuarioService.deleteUsuario(id).subscribe(() => {
+      this.loadUsuarios();
+    })
+  }
+
 }
